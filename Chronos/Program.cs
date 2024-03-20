fa826d0ec7f8a99e24bb0adfda66048b735a6f54
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-20 19:08:56 +0000

    Instancia dois cronómetros

---
+++ Chronos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System Diagnostic;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1= new Stopwatch();
+            Stopwatch crono2= new Stopwatch();
+        }
+    }
+}
