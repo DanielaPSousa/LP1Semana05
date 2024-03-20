17c478335b3bdfe22468867b009ac729b5985b89
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-20 21:00:03 +0000

    Numero de dados e a semente

---
+++ RanDice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d= int.Parse(args[0]);
+            int s= int.Parse(args[1]);
+        }
+    }
+}
