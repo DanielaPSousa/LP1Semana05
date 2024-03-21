e454af205acf89d1499603b62ae2939b9266fd23
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-21 22:42:25 +0000

    Criado projeto MyGame

---
+++ MyGamee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGamee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
