16f93962079175e556aa6773b9ed172d99cb43ff
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-22 19:34:14 +0000

    Criada uma array para os inimigos

--- MyGamee/Program.cs
+++ MyGamee/Program.cs
@@ -7,6 +7,7 @@ namespace MyGamee
         static void Main(string[] args)
         {
             int enemy man = int.Parse(args[0]);
+            Enemy[] Enemies= new Enemy[enemy c1];
         }
     }
 }
