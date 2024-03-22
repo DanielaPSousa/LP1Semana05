0c43ee7271c881f68d366803cbd952d7e3fe53e5
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-22 19:58:02 +0000

    Metodo PickupPowerUp criado com inimigo que pode aumentar a saude mas que não exceda 100

--- MyGamee/Enemy.cs
+++ MyGamee/Enemy.cs
@@ -4,42 +4,50 @@ namespace MyGamee
 {
     public class Enemy
     {
-        private string name;
-        private float shield;
-        private float health;
+    private string name;
+    private float shield;
+    private float health;
 
-        public Enemy(string name)
+    public Enemy(string name)
+    {
+        this.name= name; 
+        health= 100;
+        shield= 0;
+    }
+    public string GetName()
+    {
+        return name;
+    }
+    public void TakeDamage(float damage)
+    {
+        shield -= damage;
+        if (shield < 0)
         {
-            this.name= name; 
-            health= 100;
+            float damageStillToInflict = -shield ;
             shield= 0;
+            health-= damageStillToInflict;
+            if (health < 0) health= 0;
         }
-        public string GetName()
+    }
+    public float GetHealth()
+    {
+        return health;
+    }
+    public float GetShield()
+    {
+        return shield;
+    }
+    public void SetName(string newName)
+    {
+        name= newName;
+    }
+    public void PickupPowerUp(powerUp PowerUp float value)
+    {
+        if (powerUp == PowerUp.health)
         {
-            return name;
+            health= Math.Min(100, health + value);
         }
-        public void TakeDamage(float damage)
-        {
-            shield -= damage;
-            if (shield < 0)
-            {
-                float damageStillToInflict = -shield ;
-                shield= 0;
-                health-= damageStillToInflict;
-                if (health < 0) health= 0;
-            }
-        }
-        public float GetHealth()
-        {
-            return health;
-        }
-        public float GetShield()
-        {
-            return shield;
-        }
-        public void SetName(string newName)
-        {
-            name= newName;
-        }    
+    }
+
     }   
 }
