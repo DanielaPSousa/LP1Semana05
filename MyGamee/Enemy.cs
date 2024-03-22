50e5e4e4f641003a504a7fc26f084d07e4421438
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-22 16:32:10 +0000

    Adicionado o ficheiro Enemy e meti o Ex.6 e 7 la

---
+++ MyGamee/Enemy.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace MyGamee
+{
+    public class Enemy
+    {
+        private string name;
+        private float shield;
+        private float health;
+
+        public Enemy(string name)
+        {
+            this.name= name; 
+            health= 100;
+            shield= 0;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield ;
+                shield= 0;
+                health-= damageStillToInflict;
+                if (health < 0) health= 0;
+            }
+        }
+            
+    }   
+}
